d87ebcbb48896268ead02732368c010c745cec25
Astraz2 <thomas.david2603@gmail.com>
2026-01-13 09:17:22 +0100

    Ajout de la fonction main avec création de deux personnes

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "type_def.h"
+#include "utils.h"
+
+int main() {
+    personne* john = creerPersonne('D', 'J', 1990, 1, nullptr, nullptr, nullptr);
+    personne* jane = creerPersonne('S', 'J', 1992, 2, nullptr, nullptr, nullptr);
+}
