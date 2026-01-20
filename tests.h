de3f38c4809f2f7e1c2b02d6e0f262a9ab1250a0
Astraz2 <thomas.david2603@gmail.com>
2026-01-20 21:51:45 +0100

    Charles la prochaine fois que tu écrase ce que j'ai fait je t'en colle une >:(

---
+++ tests.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void testAfficherPersonne();
+void testVerifierMariage();
+void testAncetre ();
+void testGeneration();
+void testFrereSoeur();
+void testTailleArbre();
+void testMariagePossible();
+void testSave();
